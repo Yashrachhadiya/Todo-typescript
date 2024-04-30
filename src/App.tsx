049b6220eb8e2c31a5e4da4c5079a82305f8ee39
Yashrachhadiya <yashr@webelight.co.in>
2024-04-30 13:22:49 +0530

    Add rendring functionality of tasks

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App:React.FC = () =>  {
   return (
     <>
      <Header/>
-     <br />
+     
      <UserInput/>
     </>
   )
--- src/Comonents/Heading.tsx
+++ src/Comonents/Heading.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
     <>
-    <header className="bg-gray-800 py-4">
+    <header className="bg-gray-800 py-8">
       <div className="container mx-auto px-4">
-        <h1 className="text-2xl text-white font-semibold text-center">My To-Do App</h1>
+        <h1 className="text-3xl text-white font-bold text-center">My To-Do App</h1>
       </div>
     </header>
     </>
